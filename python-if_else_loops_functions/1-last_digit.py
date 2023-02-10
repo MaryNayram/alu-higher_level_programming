6305e8ff3411d19313ffab31f70de8724fb10aae
MaryNayram <m.doh@alustudent.com>
2023-02-10 08:57:11 -0800

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,10 +7,10 @@ if number < 0:
     last = number % -10
 else:
     last = number % 10
-    
+
 if last > 5:
     print("{:d} and is greater than 5".format(last))
-elif last == 0:    
-    print("{:d} and is 0".format(last)) 
+elif last == 0:
+    print("{:d} and is 0".format(last))
 else:
     print("{:d} and is less than 6 and not 0".format(last))
