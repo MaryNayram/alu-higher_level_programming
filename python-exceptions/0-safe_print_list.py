fa0c1652fc3aa55274f020b50fa8b02a44400cd5
MaryNayram <m.doh@alustudent.com>
2023-03-10 06:11:14 -0800

    commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-
-    for i in range(x):
+    Total = 0
+    for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
+            Total += 1
         except IndexError:
-            print()
-            return i
-
+            break
     print()
-    return i + 1
+    return Total
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-
-    new = []
-
+    new_list = [0]*list_length
     for i in range(list_length):
+        answer = 0
         try:
-            new.append(my_list_1[i] / my_list_2[i])
-        except ZeroDivisionError:
-            print("division by 0")
-            new.append(0)
-        except IndexError:
-            print("out of range")
-            new.append(0)
+            answer = (my_list_1[i] / my_list_2[i])
         except TypeError:
             print("wrong type")
-            new.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
         finally:
-            pass
-    return
+            new_list[i] = answer
+    return new_list
