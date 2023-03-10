9fbf047b20e37f4467c237c312a1f5b78a63f828
MaryNayram <m.doh@alustudent.com>
2023-03-10 08:07:37 -0800

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    import hidden_4
-    names_list = dir(hidden_4)
-    for name in names_list:
-        if name[0] != '_':
-            print("{}".format(name)
+
+import hidden_4
+
+if __name__ == "__main__":
+    for content in dir(hidden_4):
+        if content[:2] != "__":
+            print("{:s}".format(content))
