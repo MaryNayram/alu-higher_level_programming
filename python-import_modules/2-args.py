f4ac3aa853d6c11dbac5350e6a9ab3d0fee15fb5
MaryNayram <m.doh@alustudent.com>
2023-03-10 05:34:46 -0800

    committed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     import sys
-
-    print("{:d} argument".format(len(sys.argv) - 1), end="")
+    if (len(sys.argv) - 1) != 1:
+        print("{} arguments".format(len(sys.argv) - 1), end='')
+    else:
+        print("{} argument".format(len(sys.argv) - 1), end='')
     if len(sys.argv) == 1:
         print(".")
-    elif len(sys.argv) == 2:
-        print(":")
     else:
-        print("s:")
-
-    for i in range(len(sys.argv) - 1):
-        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+        print(":")
+    for count, argument in enumerate(sys.argv):
+        if (count == 0):
+            continue
+        print("{}: {}".format((count), argument))
