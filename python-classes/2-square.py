8a731a69d04e735f3c7e4fe7ab4ffa4a61fc4ac3
MaryNayram <m.doh@alustudent.com>
2023-03-17 04:26:29 -0700

    python-classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""creates class  with private instance attribute size"""
+
+
 class Square:
+    """defines class and
+present private instance attribute size with validation."""
+
     def __init__(self, size=0):
-        if isinstance(size, int) is False:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
