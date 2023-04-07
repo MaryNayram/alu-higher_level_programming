9fbc4ef4d3019b62bf27b524185a9b3632b0bb94
MaryNayram <m.doh@alustudent.com>
2023-04-07 12:17:33 +0000

    commit

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,14 @@
-emacs #!/usr/bin/python3
-"""
-Python script to send POST request to given URL with given email as parameter
-"""
-if __name__ == "__main__":
-    from urllib import request, parse
-    from sys import argv
-    if argv[2]:
-        data_1 = {}
-        data_1['email'] = argv[2]
-        data_2 = parse.urlencode(data_1)
-        data = data_2.encode("UTF-8")
-    if argv[1]:
-        req = request.Request(argv[1], data)
-        with request.urlopen(req) as response:
-            content = response.read()
-            print(content.decode("UTF-8"))
+#!/usr/bin/python3
+"""sends a POST request to the passed URL with the email as a parameter"""
+
+
+if __name__ == '__main__':
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+    import sys
+    data = urlencode({"email": sys.argv[2]
+                      }).encode("ascii")
+
+    request = Request(sys.argv[1], data)
+    with urlopen(request) as response:
+        print(response.read().decode('utf-8'))
