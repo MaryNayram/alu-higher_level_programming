c97fac9fb05dccabff60f6090662da3745bfa8e7
MaryNayram <m.doh@alustudent.com>
2023-06-09 13:48:10 -0700

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,8 @@
-// script that reads and prints the content of a file
+#!/usr/bin/node
+
+let filename = process.argv[2];
 const fs = require('fs');
-const myArgs = process.argv.slice(2);
-fs.readFile(myArgs[0], 'utf-8', function (err, data) {
+fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
